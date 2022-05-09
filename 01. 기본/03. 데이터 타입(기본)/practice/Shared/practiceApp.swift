ad21cd4d7994fe4f452fb9c66a68d658a1fd8be3
Jin418code <jin41879@gmail.com>
2022-05-09 21:52:12 +0900

    1

---
+++ 01. 기본/03. 데이터 타입(기본)/practice/Shared/practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceApp.swift
+//  Shared
+//
+//  Created by JinHan Choi on 2022/05/09.
+//
+
+import SwiftUI
+
+@main
+struct practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
