a8e8f83fb1d9f64d6cd5957322bc661d18e70d46
Jin418code <jin41879@gmail.com>
2022-05-12 01:01:36 +0900

    studying

--- 01. 기본/04. 데이터 타입(고급)/practice/5.1. 기본 열거형 .playground/Contents.swift
+++ 01. 기본/04. 데이터 타입(고급)/practice/5.1. 기본 열거형 .playground/Contents.swift
@@ -1,1 +1,23 @@
+enum School {
+    case primary
+    case elementary
+    case middle
+    case high
+    case college
+    case university
+}
 
+// 각 항목은 그 자체가 고유의 값이며, 한 줄에 모두 표현 가능하다.
+
+enum School {
+    case primary, elementary, middle, high, college, university
+}
+
+// 열거형 변수의 생성 및 값 변경
+var highestEducationLevel: School = School.university
+
+// 위 코드와 같은 표현
+var highestEducationLevel: school = .university
+
+// 같은 타임인 School 내부의 항복으로만 highestEducationLevel의 값을 변경 가능함
+highestEducationLevel = .graduate
