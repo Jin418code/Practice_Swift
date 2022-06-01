b7614d62c8f10ec563dfb2c79519d053d9702b40
Jin418code <jin41879@gmail.com>
2022-06-01 20:56:07 +0900

    studying

---
+++ 04. App만들기(기초)/1. 주사위게임/DiceGame/DiceGame/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  DiceGame
+//
+//  Created by JinHan Choi on 2022/06/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstImageView: UIImageView!
+    @IBOutlet weak var secondImageView: UIImageView!
+    
+    var diceArray: [UIImage] = [#imageLiteral(resourceName: "black1"), #imageLiteral(resourceName: "black2"), #imageLiteral(resourceName: "black3"), #imageLiteral(resourceName: "black4"), #imageLiteral(resourceName: "black5"), #imageLiteral(resourceName: "black6")]
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func rollButtonTapped(_ sender: UIButton) {
+        firstImageView.image = diceArray.randomElement()
+        
+        
+        secondImageView.image = diceArray.randomElement()
+    }
+    
+    
+    
+}
+
+
---
+++ 04. App만들기(기초)/2. 가위바위보게임/RPS Game/RPS Game/Rps.swift
@@ -0,0 +1,15 @@
+//
+//  Rps.swift
+//  RPS Game
+//
+//  Created by JinHan Choi on 2022/06/01.
+//
+
+import Foundation
+
+// 원시값(RawValue)설정
+enum Rps: Int {
+    case rock
+    case paper
+    case scissors
+}
